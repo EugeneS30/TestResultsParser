cfe508e9b1962aa0622fc064525d93c33bfe5a22
Eugene Shragovich <shragovich@gmail.com>
2015-05-28 12:06:07 +0800

    updated startBuildNumber to 389

--- conf/Conf.py
+++ conf/Conf.py
@@ -11,4 +11,4 @@ class Configuration(object):
     CSVFilePath = "C:\\Temp"
     resultsCSV = "results.csv"
     resultsCSVTemp = "resultsTemp.csv"
-    startBuildNumber = 388
+    startBuildNumber = 389
